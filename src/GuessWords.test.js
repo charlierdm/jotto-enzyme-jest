c8eba5e087ab131ef62d4eee0c093d2dc85ee41f
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2021-12-23 15:07:47 +0000

    written GuessWords describe blocks with passing tests

--- src/GuessWords.test.js
+++ src/GuessWords.test.js
@@ -16,6 +16,21 @@ test('does not throw a warning with expected props', () => {
   checkProps(GuessedWords, defaultProps)
 })
 
-test('renders without error', () => {
+describe('if there are no words guessed', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] })
+  })
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words')
+    expect(component.length).toBe(1)
+  })
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions')
+    expect(instructions.text().length).not.toBe(0)
+  })
+})
+
+describe('if there are words guessed', () => {
 
 })
