4e45b744922034b83293354cd21c7d95fa5785a2
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2021-12-23 00:32:36 +0000

    test setup and created testUtils

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,21 @@
 import Enzyme, { shallow } from 'enzyme'
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17'
 
+import { findByTestAttr } from '../test/testutils'
 import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() })
 
+const setup = (props={}) => {
+  return shallow(<Congrats {...props} />)
+} 
+
 test('renders without error', () => {
-  
+
+})
+test('renders no text when success prop is false', () => {
+
+})
+test('renders non-empty congrats message when success prop is true', () => {
+
 })
