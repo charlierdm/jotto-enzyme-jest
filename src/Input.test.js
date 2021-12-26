f3e151a052b67bb300ac48754c7e305fa8306ddf
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2021-12-26 11:23:48 +0000

    added jest mock to test desctructured useSate hook

--- src/Input.test.js
+++ src/Input.test.js
@@ -4,6 +4,13 @@ import { shallow } from 'enzyme'
 import Input from './Input'
 import React from "react";
 
+const mockSetCurrentGuess = jest.fn()
+
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useState: (initialGuess) => [initialGuess, mockSetCurrentGuess] 
+}))
+
 const setup = (secretWord='party') => {
   return shallow(<Input secretWord={secretWord} />)
 }
@@ -20,9 +27,6 @@ test('it does not give a warning when passed the correct props', () => {
 
 describe('state controlled input field', () => {
   test('state updates with value of the input box upon change', () => {
-    const mockSetCurrentGuess = jest.fn()
-    React.useState = jest.fn(() => [ '', mockSetCurrentGuess ])
-
     const wrapper = setup()
     const inputBox = findByTestAttr(wrapper, 'input-box')
     const mockEvent = { target: { value: 'train' }}
