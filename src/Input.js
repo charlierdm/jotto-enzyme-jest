28a480724212dfab2e3fe8aab25e04358f371f4c
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2021-12-26 11:00:48 +0000

    created Input component form

--- src/Input.js
+++ src/Input.js
@@ -1,9 +1,21 @@
-import react from "react"
+import React from "react"
 import propTypes from "prop-types"
 
 const Input = ({ secretWord }) => {
+  const [currentGuess, setCurrentGuess] = React.useState('')
   return (
     <div data-test='component-input'>
+      <form className='input-form'>
+        <input
+          data-test='input-box'
+          className="input-box"
+          type='text'
+          placeholder="enter guess"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button data-test='submit-button' type="submit" >Submit</button>
+      </form>
     </div>
   )
 }
