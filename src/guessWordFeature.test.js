bc8eef5f2820eb40d03df55b704871a79703a86e
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-01-23 15:56:12 +0000

    wrote test expectancies for guessWordFeature test

--- src/guessWordFeature.test.js
+++ src/guessWordFeature.test.js
@@ -9,7 +9,7 @@ const setup = (state = {}) => {
   const wrapper = mount(<App />)
 
   const inputBox = findByTestAttr(wrapper, 'input-box')
-  inputBox.simulate('change', { targert: { value: 'train' } })
+  inputBox.simulate('change', { target: { value: 'train' } })
   const submitButton = findByTestAttr(wrapper, 'submit-button')
   submitButton.simulate('click', { preventDefault() {}})
 
@@ -32,9 +32,41 @@ describe('no words guessed', () => {
 })
 
 describe('some words have guessed', () => {
-  
+  let wrapper
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{guessedWord: 'train', letterMatchCount: 3}]
+    })
+  })
+  test('creates a GuessedWords table with 2 rows', () => {
+    const guessedWordsRows = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordsRows).toHaveLength(2)
+  })
 })
 
 describe('guess the secret word', () => {
-  
+  let wrapper
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: 'party',
+      success: false,
+      guessedWords: [{guessedWord: 'part', letterMatchCount: 4}]
+    })
+  // add value to input
+  const inputBox = findByTestAttr(wrapper, 'input-box')
+  inputBox.simulate('change', {target: {value: 'party'}})
+  // submit secret word
+  const submitButton = findByTestAttr(wrapper, 'submit-button')
+  submitButton.simulate('click', {preventDefault() {}})
+  })
+  test('adds row to guessedWords table', () => {
+    const guessedWordRows = findByTestAttr(wrapper, 'guessed-word')
+    expect(guessedWordRows).toHaveLength(3)
+  })
+  test('displays congrats component', () => {
+    const congrats = findByTestAttr(wrapper, 'component-congrats')
+    expect(congrats.text().length).toBeGreaterThan(0)
+  })
 })
