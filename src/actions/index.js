8459cfac58cb6471a7504f92262b13c56a968771
Charlie Davis-Miller <charlie.davismiller@gmail.com>
2022-02-08 22:14:36 +0000

    created CORRECT_GUESS action creator and test

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS'
+}
+
+export function correctGuess() {
+  return {type: actionTypes.CORRECT_GUESS}
+}
+
 export const getSecretWord = () => {
   // TODO: write actual action in redux/context sections
   return axios.get('http://localhost:3030')
